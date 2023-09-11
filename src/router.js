ed9fb4bc0c373cba54eae2172cdc3b3ea3a8bbc4
Alex Timoncini <126895091+AlexTimoncini@users.noreply.github.com>
2023-09-11 17:03:35 +0200

    prepared workInProgress view

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Homepage from './views/HomePage.vue'
+import Homepage from './views/HomePage.vue';
+import WorkInProgress from './views/WorkInProgress.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -8,6 +9,11 @@ const router = createRouter({
             path: '/',
             name: 'Homepage',
             component: Homepage
+        },
+        {
+            path: '/workinprogress',
+            name: 'WorkInProgress',
+            component: WorkInProgress
         },
     ],
 });
---
+++ src/views/WorkInProgress.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+    name: 'WorkInProgress'
+}
+</script>
+
+<template>
+    <h1>Work in progress</h1>
+</template>
+
+<style lang="scss" scoped>
+    
+</style>
