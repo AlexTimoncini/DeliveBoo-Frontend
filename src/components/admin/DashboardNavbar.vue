de4a2d208d4d2e318fa8c7fc0af3d0fe856b2a95
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-09-15 12:02:12 +0200

    removed rows and cols from components

--- src/components/admin/DashboardNavbar.vue
+++ src/components/admin/DashboardNavbar.vue
@@ -1,22 +1,18 @@
 <template>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col md-9 col-10 my_navbar d-flex align-items-center">
-                <ul class="d-flex justify-content-between align-items-center m-0 w-100">
-                    <li>
-                        <h3>My Account</h3>
-                    </li>
-                    <li>
-                        <div>
-                            <span class="me-3">Restaurant Name</span>
-                            <div class="logo d-inline-block">
-                                <img class="img-fluid" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png" alt="Logged user logo">
-                            </div>
-                        </div>
-                    </li>
-                </ul>
-            </div>
-        </div>
+    <div class="my_navbar d-flex align-items-center px-3">
+        <ul class="d-flex justify-content-between align-items-center m-0 p-0 w-100">
+            <li>
+                <h3>My Account</h3>
+            </li>
+            <li>
+                <div>
+                    <span class="me-3">Restaurant Name</span>
+                    <div class="logo d-inline-block">
+                        <img class="img-fluid" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/1200px-McDonald%27s_Golden_Arches.svg.png" alt="Logged user logo">
+                    </div>
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -38,6 +34,7 @@ export default {
     div.my_navbar {
         background-color: $secYellow;
         height: 80px;
+        box-shadow: rgba(33, 35, 38, 0.1) 0px 10px 10px -10px;
 
         div.logo {
             width: 50px;
