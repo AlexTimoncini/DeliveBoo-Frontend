f004c52b5a221052e8c990ac84705bbfd0be1cf5
MarioSantoro <mariosantoro124@gmail.com>
2023-09-18 17:03:50 +0200

    added config for storing the library pinia

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import { router } from './router';
+
+
+import { createApp, markRaw } from 'vue'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+
 import './axios'
+import { router } from './router'
+
 import * as bootstrap from 'bootstrap'
-createApp(App).use(router).mount('#app');
+const pinia = createPinia();
+
+pinia.use(({ store }) => {
+    store.router = markRaw(router);
+})
+const app = createApp(App)
+
+app.use(pinia);
+app.use(router);
+
+app.mount('#app');
