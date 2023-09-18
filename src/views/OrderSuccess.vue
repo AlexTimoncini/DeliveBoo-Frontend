080ef7aeb683a47e72229bf17ed3d2cf22b27b2b
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-09-18 12:06:10 +0200

    fixed code indentation

--- src/views/OrderSuccess.vue
+++ src/views/OrderSuccess.vue
@@ -9,23 +9,23 @@ export default{
     <div class="wrapperAlert">
         <div class="contentAlert">
             <div class="topHalf">
-                <p>
-                <svg viewBox="0 0 512 512" width="100" title="check-circle">
-                    <path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z" />
-                </svg>
-                </p>
                 <h1>Congratulations! You’ve ordered a Boo-tiful meal!</h1>
+                <p>
+                    <svg viewBox="0 0 512 512" width="100" title="check-circle">
+                        <path d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z" />
+                    </svg>
+                </p>
                 <ul class="bg-bubbles">
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
-                <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
                 </ul>
             </div>
             <div class="bottomHalf">
@@ -88,10 +88,10 @@ div.container-fluid {
 
             p {
                 margin-bottom: 30px;
-            }
-
-            svg {
-                fill: white;
+                
+                svg {
+                    fill: white;
+                }
             }
 
             h1 {
