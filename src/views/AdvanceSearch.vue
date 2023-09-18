dff3c96302914262754b1e0d3de39cf0ff1cacbe
EmanuelaPau <uapame@gmail.com>
2023-09-18 11:06:38 +0200

    Fix input range and searchbar syle in chrome

--- src/views/AdvanceSearch.vue
+++ src/views/AdvanceSearch.vue
@@ -31,54 +31,54 @@ export default {
         }
     },
     methods: {
-        getTypes(){
+        getTypes() {
             axios.get(store.ApiUrl + 'types')
-                .then( (response) => {
+                .then((response) => {
                     this.allTypes = response.data.data;
                     this.visibleTypesXl = response.data.data.slice(3, 11);
                     this.visibleTypesLarge = response.data.data.slice(3, 10);
                     this.visibleTypesSmall = response.data.data.slice(3, 8);
-            })
+                })
                 .catch(function (error) {
                     console.log('ops');
-            });
+                });
         },
-        getBestRestaurants(){
+        getBestRestaurants() {
             axios.get(store.ApiUrl + 'restaurants/bestSeller')
-                .then( (response) => {
+                .then((response) => {
                     store.bestSellers = response.data.data;
                 })
                 .catch(function (error) {
                     console.log(error);
-            });
+                });
         },
-        getCategories(){
+        getCategories() {
             axios.get(store.ApiUrl + 'categories')
-                .then( (response) => {
+                .then((response) => {
                     this.allCategories = response.data.data;
                     this.visibleCategories = response.data.data.slice(1, 10);
                     console.log(this.visibleCategories);
-            })
+                })
                 .catch(function (error) {
                     console.log(error);
-            });
+                });
         },
-        searchRestaurant(){
+        searchRestaurant() {
             this.params.searchbar = this.search;
             let query = encodeURIComponent(JSON.stringify(this.params));
             axios.get(store.ApiUrl + 'search/advance/' + query)
-            .then( (response) => {
-                this.filteredRestaurants = response.data.data;
-                console.log(this.filteredRestaurants);
-            })
-            .catch(function (error) {
+                .then((response) => {
+                    this.filteredRestaurants = response.data.data;
+                    console.log(this.filteredRestaurants);
+                })
+                .catch(function (error) {
                     console.log(query);
                     console.log(error);
-            });
+                });
         },
-        
+
     },
-    mounted(){
+    mounted() {
         this.getTypes();
         this.getBestRestaurants();
         this.getCategories();
@@ -97,8 +97,7 @@ export default {
             <h1 class="me-3 mb-0 my_search_title">
                 What do you boona eat?
             </h1>
-            <div class="search-bar "
-                @keyup.enter="searchRestaurant()">
+            <div class="search-bar " @keyup.enter="searchRestaurant()">
                 <label for="home-searchbar">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 101 101" id="search">
                         <path
@@ -234,7 +233,7 @@ export default {
             <!-- Main -->
 
             <div class="my_main col-12 col-md-9 mt-4">
-                <div  class="container">
+                <div class="container">
                     <div v-if="params == null" class="row">
                         <!-- Best sellers  -->
                         <div class="col-12 mb-4">
@@ -245,14 +244,14 @@ export default {
                         <!-- New in town -->
                         <div class="col-12 mb-4">
                             <h3 class="cards_title">New in Town</h3>
-                            <Slider :slides="store.bestSellers" :autoplay="0" :pagination="false" :title="true" 
-                            :navigation="true" />
+                            <Slider :slides="store.bestSellers" :autoplay="0" :pagination="false" :title="true"
+                                :navigation="true" />
                         </div>
                         <!-- Free delivery -->
                         <div class="col-12 mb-4">
                             <h3 class="cards_title">Free delivery under 10 bucks</h3>
-                            <Slider :slides="store.bestSellers" :autoplay="0" :pagination="false" :title="true" 
-                            :navigation="true" />
+                            <Slider :slides="store.bestSellers" :autoplay="0" :pagination="false" :title="true"
+                                :navigation="true" />
                         </div>
                         <!-- Discover more -->
                         <div class="col-12 mb-4">
@@ -349,8 +348,9 @@ export default {
         height: 100%;
         aspect-ratio: 1/1;
         cursor: pointer;
+        width: 50px;
         padding: 0 0.5rem;
-        background-color: white;
+        background-color: $priGreen;
         border: 1px 0 1px 1px solid $secYellow;
         border: 1px solid rgb(237, 237, 237);
         border-radius: 10px 0 0 10px;
@@ -359,7 +359,7 @@ export default {
         svg {
             height: 100%;
             aspect-ratio: 1/1;
-            fill: $priGreen;
+            fill: $fontWhite;
         }
     }
 }
@@ -372,7 +372,7 @@ export default {
         font-weight: 700;
     }
 
-    .pill-container{
+    .pill-container {
         column-gap: 1rem;
     }
 
@@ -478,10 +478,15 @@ div.search-page-container {
                     pointer-events: auto;
                 }
 
-                input[type="range"]::-webkit-slider-thumb {
+                input[type=range]::-webkit-slider-thumb {
+                    -webkit-appearance: none;
+                    border: none;
+                    height: 20px;
+                    width: 20px;
+                    border-radius: 50%;
                     background-color: $priGreen;
                     border: 1px solid white;
-
+                    margin-top: -7px;
                 }
 
                 input[type="range"]::-moz-range-thumb {
