f249d453fb079c85b96dac8c1772ee873550cde6
EmanuelaPau <uapame@gmail.com>
2023-09-19 09:57:11 +0200

    Add cart button click feedback

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -9,7 +9,8 @@
                     <div class="card__info-title">
                         <h3>{{ dish.name }}</h3>
                         <p>{{ dish.description }}</p>
-                        <p ><span class="me-2" v-for="ingredient, index in dish.ingredients">{{ ingredient.name }} {{ ((index + 1) == dish.ingredients.length) ? '' : '-' }} </span></p>
+                        <p><span class="me-2" v-for="ingredient, index in dish.ingredients">{{ ingredient.name }} {{ ((index
+                            + 1) == dish.ingredients.length) ? '' : '-' }} </span></p>
                     </div>
                 </div>
                 <div class="card__price col-4">
@@ -24,9 +25,11 @@
 </template>
 
 <script>
-export default{
+export default {
     name: 'Card',
-
+    data: () => ({
+        isDishAddToCart: false,
+    }),
     props: {
         dish: Object
     },
@@ -34,99 +37,118 @@ export default{
 </script>
 
 <style lang="scss" scoped>
-    @use '../styles/partials/variables' as *;
-    @use '../styles/partials/mixins' as *;
-    
-    .card {
-        overflow: hidden;
-        border-radius: 25px;
-        background-color: $secYellow;
-        border: 5px solid white;
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
 
-        .card__image {
-            position: relative;
-            height: 300px;
+.card {
+    overflow: hidden;
+    border-radius: 25px;
+    background-color: $secYellow;
+    border: 5px solid white;
 
-            img {
-                border-bottom-right-radius: 30px;
-                transform: rotate(-5deg) translate(-25px, -25px);
-                position: absolute;
-                width: 90%;
-                height: 100%;
-                object-fit: cover;
-                object-position: center;
-                transition: all .3s ease;
-                box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
-                outline: 10px solid white;
+    .card__image {
+        position: relative;
+        height: 300px;
 
-                &:hover {
-                    transform: rotate(0deg) translate(25px, 25px) scale(1.3);
-                }
+        img {
+            border-bottom-right-radius: 30px;
+            transform: rotate(-5deg) translate(-25px, -25px);
+            position: absolute;
+            width: 90%;
+            height: 100%;
+            object-fit: cover;
+            object-position: center;
+            transition: all .3s ease;
+            box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+            outline: 10px solid white;
+
+            &:hover {
+                transform: rotate(0deg) translate(25px, 25px) scale(1.3);
             }
         }
+    }
 
-        .card__info {
-            padding: 1rem;
-            z-index: 1;
+    .card__info {
+        padding: 1rem;
+        z-index: 1;
 
-            .card__info-title {
-                height: 250px;
-                display: flex;
-                flex-direction: column;
-                justify-content: space-between;
+        .card__info-title {
+            height: 250px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
 
-                h3 {
-                    font-size: 1.5rem;
-                    font-weight: bold;
-                }
-
-                p {
-                    font-weight: 600;
-                }
-
-                span {
-                    color: $detGrey;
-                }
+            h3 {
+                font-size: 1.5rem;
+                font-weight: bold;
             }
-        }
-
-        .card__price {
-            @include flex(column, end, center, no-wrap);
-            text-align: center;
-            z-index: 1;
 
             p {
-                background-color: white;
-                padding: .5rem 1rem;
-                border-radius: 25px;
                 font-weight: 600;
-                box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
             }
 
-            button {
-                background-color: $priGreen;
-                width: 70px;
-                box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
-                border-radius: 50%;
-                padding: .8rem;
-                border: none;
-                margin-bottom: 1rem;
+            span {
+                color: $detGrey;
+            }
+        }
+    }
+
+    .card__price {
+        @include flex(column, end, center, no-wrap);
+        text-align: center;
+        z-index: 1;
+
+        p {
+            background-color: white;
+            padding: .5rem 1rem;
+            border-radius: 25px;
+            font-weight: 600;
+            box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+        }
+
+
+
+        button {
+            background-color: $priGreen;
+            width: 70px;
+            box-shadow: rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px;
+            border-radius: 50%;
+            padding: .8rem;
+            border: none;
+            margin-bottom: 1rem;
+            transition: all .3s ease;
+            border: 5px solid white;
+
+            position: relative;
+
+            .plus-one {
+                position: absolute;
+
+            }
+
+            .animation-plus-one {}
+
+            img {
                 transition: all .3s ease;
-                border: 5px solid white;
+            }
+
+            &:hover {
+                cursor: pointer;
+                background-color: #009e5f;
 
                 img {
-                    transition: all .3s ease;
+                    transform: scale(1.1);
                 }
+            }
 
-                &:hover {
-                    cursor: pointer;
-                    background-color: #009e5f;
+            &:active {
+                background-color: #12b350;
 
-                    img {
-                        transform: scale(1.1);
-                    }
+                img {
+                    transform: scale(.9);
                 }
             }
         }
     }
+}
 </style>
