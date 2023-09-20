1c61a019cf3f3b381d48e0d0b5d441addab958c9
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-09-20 11:27:09 +0200

    added deleteDish method with delete api call

--- src/views/admin/Dishes.vue
+++ src/views/admin/Dishes.vue
@@ -26,10 +26,10 @@
 
                             </div>
                             <!-- Cards  -->
-                            <div class="col-12 col-md-6 col-lg-4 p-0 p-md-3" v-for="dish in authStore.user.dishes">
+                            <div class="col-12 col-md-6 col-lg-4 p-0 p-md-3" v-for="dish in authStore.user.dishes" :key="dish.id">
                                 <div class="dish-card card-container p-3">
                                     <div class="card-buttons-container d-flex justify-content-end">
-                                        <div class="card-button btn">
+                                        <div class="card-button btn" @click="deleteDish(dish.id)">
                                             <svg viewBox="0 0 24 24">
                                                 <path
                                                     d="M 10 2 L 9 3 L 4 3 L 4 5 L 5 5 L 5 20 C 5 20.522222 5.1913289 21.05461 5.5683594 21.431641 C 5.9453899 21.808671 6.4777778 22 7 22 L 17 22 C 17.522222 22 18.05461 21.808671 18.431641 21.431641 C 18.808671 21.05461 19 20.522222 19 20 L 19 5 L 20 5 L 20 3 L 15 3 L 14 2 L 10 2 z M 7 5 L 17 5 L 17 20 L 7 20 L 7 5 z M 9 7 L 9 18 L 11 18 L 11 7 L 9 7 z M 13 7 L 13 18 L 15 18 L 15 7 L 13 7 z">
@@ -96,6 +96,7 @@
 </template>
 
 <script>
+import axios from 'axios';
 import DashboardSidebar from '../../components/admin/DashboardSidebar.vue';
 import DashboardNavbar from '../../components/admin/DashboardNavbar.vue';
 export default {
@@ -103,7 +104,22 @@ export default {
     data() {
 
     },
-    components: { DashboardSidebar, DashboardNavbar }
+    components: { DashboardSidebar, DashboardNavbar },
+
+    methods: {
+        deleteDish(dishID) {
+            if (window.confirm('Are you sure you want to delete this dish?')) {
+                axios.delete(`/api/delete/${dishID}`)
+                .then((response) => {
+                    console.log(response);
+                    window.location.href = '/admin/dishes';
+                })
+                .catch(function (error) {
+                    console.log(error);
+                })
+            }
+        }
+    }
 }
 </script>
 
