35b32dedf98bc19056e2ce1179fa644b7a375f93
MarioSantoro <mariosantoro124@gmail.com>
2023-09-26 15:43:49 +0200

    add message error on payment

--- src/views/CheckOut.vue
+++ src/views/CheckOut.vue
@@ -52,10 +52,15 @@
                             {{ error }}
                         </div> -->
                         <form @submit.prevent="payWithCreditCard" class="d-flex flex-column">
-                            <div class="form-group my-2">
-                                <label>Nome</label>
-                                <input type="text" v-model="name" class="form-control" placeholder="Inserisci Nome"
-                                    required>
+                            <label for="name">Name</label>
+                            <div class="position-relative">
+                                <input type="text" name="name" id="name" v-model="name" />
+                                <div class="info-message" v-if="!errorForm.name">
+                                    <span>Enter your name.</span>
+                                </div>
+                                <div class="error-message" v-if="errorForm.name">
+                                    <span>{{ errorForm.name.join() }}</span>
+                                </div>
                             </div>
                             <div class="form-group my-2">
                                 <label>Cognome</label>
@@ -129,12 +134,19 @@ export default {
             phone_number: '',
             name: '',
             address: '',
+
+            // variabile per errori carta di credito
             error: '',
+
+
             surname: '',
             email: '',
             totalPrice: 0,
             tokenApi: '',
-            isFieldInstance: false
+            isFieldInstance: false,
+
+            // variabile per errori del form
+            errorForm: {},
         };
     },
 
@@ -238,7 +250,8 @@ export default {
                         this.$router.push({ name: 'Homepage' });
                     }
                 }).catch((error) => {
-                    console.log(error.response.data.errors)
+                    this.errorForm = error.response.data.errors;
+                    console.log(this.errorForm)
                 })
                 .catch((err) => {
                     swal("Pagamento rifiutato", "Il tuo ordine non è stato effetuato");
@@ -283,6 +296,14 @@ export default {
     .input-pay {
         height: 40px;
     }
+}
+
+div.info-message {
+    background-color: $secYellow;
+}
+
+div.error-message {
+    background-color: #f86f6f;
 }
 
 .order_review {
