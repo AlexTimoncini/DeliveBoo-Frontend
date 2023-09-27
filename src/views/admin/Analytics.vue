0bd87dd05242c904350a3ad08808eaa1102dbbe4
EmanuelaPau <uapame@gmail.com>
2023-09-27 09:54:23 +0200

    Add analytics page

--- src/views/admin/Analytics.vue
+++ src/views/admin/Analytics.vue
@@ -6,7 +6,30 @@
             </div>
             <div class="col-md-9 col-10 p-0">
                 <DashboardNavbar />
+                <div class="container my_app ">
+                    <!-- Totals-->
+                    <div class="row">
+                        <!-- Total revenues -->
+                        <div class="col-12 col-sm-6 numbers-box-container mb-3 mb-sm-0 pe-sm-4">
+                            <div class="numbers-box">
+                                <h4>Total Revenues</h4>
+                                <h2>$15.000</h2>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-sm-6 numbers-box-container ">
+                            <!-- Total Orders -->
+                            <div class="numbers-box">
+                                <h4>Total Orders</h4>
+                                <h2>1.000</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+
+
         </div>
     </div>
 </template>
@@ -25,4 +48,40 @@ export default {
 <style lang="scss" scoped>
 @use '../../styles/partials/variables' as *;
 @use '../../styles/partials/mixins' as *;
+
+.my_app::-webkit-scrollbar {
+    display: none;
+}
+
+.my_app {
+    width: 100%;
+    height: calc(100vh - 95px - 80px);
+    overflow-y: scroll;
+    padding: 2rem;
+    background-color: rgb(252, 252, 252);
+
+    -ms-overflow-style: none;
+    /* IE and Edge */
+    scrollbar-width: none;
+    /* Firefox */
+
+    .numbers-box {
+        background-color: white;
+        // height: 300px;
+        width: 100%;
+        border-radius: 10px;
+        padding: 1.5rem;
+        box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+
+        h4 {
+            color: grey;
+        }
+
+        h2 {
+            color: $priGreen;
+            font-weight: 700;
+            font-size: 2.5rem;
+        }
+    }
+}
 </style>
