78170454822d378b469451058d99ae4d694f95bf
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-09-28 11:23:27 +0200

    created empty AboutUs view

---
+++ src/views/AboutUs.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container">
+        <h1>About Us</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AboutUs'
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
