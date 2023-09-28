528c969b70933afa1c28e4b70832de3dedbae41a
EmanuelaPau <uapame@gmail.com>
2023-09-28 13:41:48 +0200

    Add about us page

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,6 +1,116 @@
 <template>
-    <div class="container">
-        <h1>About Us</h1>
+    <div class="">
+        <div class="jumbo px-3 bg-yellow">
+            <div class="container d-flex align-items-center flex-column flex-md-row">
+                <div class="col-lg-7">
+                    <h1 class="color-white">
+                        Meet our team of Programmers, Creators, <em>and Spectral Sorcerers.</em>
+                    </h1>
+                    <h5 class="d-none d-lg-block">Step into a realm where creators merge the realms of
+                        <strong>technology</strong> and
+                        the
+                        <strong>occult</strong>. In this
+                        eerie journey
+                        through the digital shadows, we unveil the arcane secrets and <strong>supernatural
+                            techniques</strong>
+                        that power our
+                        <strong> groundbreaking projects</strong>.
+                    </h5>
+                </div>
+                <div class="ms-lg-5 col-lg-4 ">
+                    <img class="container-fluid " src="../assets/mascotte/castle.png" alt="spooky castle illustration">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <h1 class="px-3 color-green title text-cent">The Team:</h1>
+            <div class="cards-container d-flex justify-content-center flex-wrap">
+
+                <div class="my_cards col-12 col-md-6 col-lg-3 p-3">
+                    <div class=" w-100 img_container bg-green mb-2">
+                        <img class="container-fluid p-0" src="../assets/mascotte/boo-alex.png" alt="">
+                    </div>
+                    <h3 class="mb-0">Alex Timoncini</h3>
+                    <h5>Code Conjurer</h5>
+                    <p><em>"Ciao a tutti dico cose qui"</em></p>
+
+                </div>
+
+                <div class="my_cards col-12 col-md-6 col-lg-3 p-3">
+                    <div class="w-100 img_container bg-yellow mb-2">
+                        <img class="container-fluid p-0" src="../assets/mascotte/boo-vito.png" alt="">
+                    </div>
+                    <h3 class="mb-0">Vito Pizzulli</h3>
+                    <h5>IT Illusionist</h5>
+                    <p><em>"Ciao a tutti dico cose qui"</em></p>
+
+                </div>
+
+                <div class="my_cards col-12 col-md-6 col-lg-3 p-3">
+                    <div class="w-100 img_container bg-green mb-2">
+                        <img class="container-fluid p-0" src="../assets/mascotte/boo-ema.png" alt="">
+                    </div>
+                    <h3 class="mb-0">Emanuela Pau</h3>
+                    <h5>Sorcerer of Software Spells</h5>
+                    <p><em>"I don't know what to say... Boo..?!"</em></p>
+                </div>
+
+                <div class="my_cards col-12 col-md-6 col-lg-3 p-3">
+                    <div class="w-100 img_container bg-yellow mb-2">
+                        <img class="container-fluid p-0" src="../assets/mascotte/boo-mario.png" alt="">
+                    </div>
+                    <h3 class="mb-0">Mario Santoro</h3>
+                    <h5>Ghost Debugger</h5>
+                    <p><em>"Ciao a tutti dico cose qui"</em></p>
+                </div>
+
+
+            </div>
+
+            <hr class=" mb-5">
+
+            <h1 class="text-center mb-5"><em>Know us better on our Linkedins</em></h1>
+            <div class="linkedin-container d-flex justify-content-center align-items-center mb-5">
+                <div class="mx-2 d-flex flex-column justify-content-center">
+                    <a href="https://www.linkedin.com/in/alex-timoncini/">
+                        <svg viewBox="0 0 24 24">
+                            <path
+                                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                        </svg>
+                    </a>
+                    <h5 class="mt-2 text-center color-green">Alex</h5>
+                </div>
+                <div class="mx-2  d-flex flex-column justify-content-center">
+                    <a href="https://www.linkedin.com/in/vito-pizzulli/">
+                        <svg viewBox="0 0 24 24">
+                            <path
+                                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                        </svg>
+                    </a>
+                    <h5 class="mt-2 text-center color-green">Vito</h5>
+                </div>
+                <div class="mx-2  d-flex flex-column justify-content-center">
+                    <a href="https://www.linkedin.com/in/emanuela-pau/">
+                        <svg viewBox="0 0 24 24">
+                            <path
+                                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                        </svg>
+                    </a>
+                    <h5 class="mt-2 text-center color-green">Ema</h5>
+                </div>
+                <div class="mx-2  d-flex flex-column justify-content-center">
+                    <a href="https://www.linkedin.com/in/mario-santoro-3781a725a/">
+                        <svg viewBox="0 0 24 24">
+                            <path
+                                d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+                        </svg>
+                    </a>
+                    <h5 class="mt-2 text-center color-green">Mario</h5>
+                </div>
+            </div>
+        </div>
+
+
     </div>
 </template>
 <script>
@@ -9,5 +119,114 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
-    
+@use '../styles/partials/variables' as *;
+@use '../styles/partials/mixins' as *;
+
+.jumbo {
+    padding-top: 2rem;
+    color: rgb(22, 22, 22);
+    margin-bottom: 2rem;
+
+    h1 {
+        font-weight: 700;
+        font-size: 3rem;
+    }
+
+    h5 {
+        font-weight: 300;
+        font-size: 1.15rem;
+
+        strong {
+            font-weight: 500;
+        }
+    }
+}
+
+.my_cards {
+    .img_container {
+        border-radius: 10px;
+    }
+
+    h3 {
+        font-weight: 600;
+    }
+
+    h5 {
+        color: rgb(90, 90, 90);
+        font-weight: 400;
+    }
+}
+
+.linkedin-container {
+    div {
+        a {
+            svg {
+                width: 80px;
+                fill: $priGreen;
+            }
+        }
+
+        a:hover {
+            svg {
+                width: 80px;
+                fill: #2eac69;
+            }
+        }
+
+        a:active {
+            svg {
+                width: 80px;
+                fill: $priGreen;
+            }
+        }
+    }
+}
+
+.color-green {
+    color: $priGreen;
+}
+
+.color-yellow {
+    color: $secYellow;
+}
+
+.bg-green {
+    background-color: $priGreen;
+}
+
+.bg-yellow {
+    background-color: $secYellow;
+}
+
+.title {
+    font-weight: 700;
+}
+
+/**MEDIA QUERIES**/
+@media (max-width: $small) {
+    .jumbo {
+        padding-top: 2rem;
+        color: rgb(22, 22, 22);
+        margin-bottom: 2rem;
+
+        h1 {
+            font-weight: 700;
+            font-size: 2rem;
+            text-align: center;
+        }
+    }
+}
+
+@media (max-width: 1400px) {
+    .jumbo {
+        padding-top: 2rem;
+        color: rgb(22, 22, 22);
+        margin-bottom: 2rem;
+
+        h1 {
+            font-weight: 700;
+            font-size: 2rem;
+        }
+    }
+}
 </style>
